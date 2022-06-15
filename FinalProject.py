f45a3de79ee26215d383d5ab62f62e01017a3f56
Nrqlah <106502785+Nrqlah@users.noreply.github.com>
2022-06-15 19:13:57 +0800

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -21,39 +21,37 @@ def default_dataset(name):
     
 
 def User_defined_dataset(chosen_name):
-  X = []
-  y = []
-  X_name = []
-  X1 = []
+    X = []
+    y = []
+    X_name = []
+    X1 = []
   
-  if chosen_name == 'Default':
-     dataset_name = st.sidebar.selectbox('Select Dataset',('Iris','Wine'))
-     X, y = default_dataset(dataset_name)
-     X_name = X
-  else:
-     upload_file = st.sidebar.file_uploader('Upload a csv',type='csv')
+    if chosen_name == 'Default':
+       dataset_name = st.sidebar.selectbox('Select Dataset',('Iris','Wine'))
+       X, y = default_dataset(dataset_name)
+       X_name = X
+    else:
+       upload_file = st.sidebar.file_uploader('Upload a csv',type='csv')
     
-     if upload_file!=None:
-        st.write(uplod_file)
-        data = pd.read_csv(upload_file)
+       if upload_file!=None:
+          st.write(uplod_file)
+          data = pd.read_csv(upload_file)
   
-        y_name = st.sidebar.selectbox('Select a y variable',sorted(data))
-        X_name = st.sidebar.multiselect('Select the x variable(s)',
-                                        sorted(data)[1],
-                                        help='You may select more than one varible')  
-        y = data.loc[:,y_name]
-        X = data.loc[:,X_name]
-        X1 = X.select_dtypes(include=['object'])
-        X2 = X.select_dtypes(exclude=['object'])
+          y_name = st.sidebar.selectbox('Select a y variable',sorted(data))
+          X_name = st.sidebar.multiselect('Select the x variable(s)',
+                                          sorted(data)[1],
+                                          help='You may select more than one varible')  
+          y = data.loc[:,y_name]
+          X = data.loc[:,X_name]
+          X1 = X.select_dtypes(include=['object'])
+          X2 = X.select_dtypes(exclude=['object'])
   
-        if sorted(X1)!=[]:
-           X1 = X1.apply(LabelEncoder().fit_transform)
-           X = pd.concat([X2,X1],axis=1)
+          if sorted(X1)!=[]:
+             X1 = X1.apply(LabelEncoder().fit_transform)
+             X = pd.concat([X2,X1],axis=1)
     
-        y = LabelEncoder().fit_transform(y)
+          y = LabelEncoder().fit_transform(y)
   
-     else:
-        st.write('Note: Please upload a csv file')
-  return X, y, X_name, X1
- 
-X, y, X_name, cat_var = User_defined_dataset(chosen)
+       else:
+          st.write('Note: Please upload a csv file')
+    return X, y, X_name, X1
