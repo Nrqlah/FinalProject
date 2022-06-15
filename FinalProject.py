8873c5df8ad047acafc670860b74836aab49fc89
Nrqlah <106502785+Nrqlah@users.noreply.github.com>
2022-06-15 18:55:54 +0800

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -25,27 +25,33 @@ def User_defined_dataset(chosen_name):
   y = []
   X_name = []
   X1 = []
-
-  upload_file = st.sidebar.file_uploader('Upload a csv',type='csv')
-  if upload_file!=None:
-    st.write(uplod_file)
-    data = pd.read_csv(upload_file)
   
-    y_name = st.sidebar.selectbox('Select a y variable',sorted(data))
-    X_name = st.sidebar.multiselect('Select the x variable(s)',
-                                   sorted(data)[1],
-                                    help='You may select more than one varible')  
-    y = data.loc[:,y_name]
-    X = data.loc[:,X_name]
-    X1 = X.select_dtypes(include=['object'])
-    X2 = X.select_dtypes(exclude=['object'])
-  
-    if sorted(X1)!=[]:
-      X1 = X1.apply(LabelEncoder().fit_transform)
-      X = pd.concat([X2,X1],axis=1)
-    
-    y = LabelEncoder().fit_transform(y)
-  
+  if choice_name == 'Default':
+     dataset_name = st.sidebar.selectbox('Select Dataset',('Iris','Wine'))
+     X, y = default_dataset(dataset_name)
+     X_name = X
   else:
-    st.write('Note: Please upload a csv file')
- return X, y, X_name, X1
+     upload_file = st.sidebar.file_uploader('Upload a csv',type='csv')
+    
+     if upload_file!=None:
+        st.write(uplod_file)
+        data = pd.read_csv(upload_file)
+  
+        y_name = st.sidebar.selectbox('Select a y variable',sorted(data))
+        X_name = st.sidebar.multiselect('Select the x variable(s)',
+                                        sorted(data)[1],
+                                        help='You may select more than one varible')  
+        y = data.loc[:,y_name]
+        X = data.loc[:,X_name]
+        X1 = X.select_dtypes(include=['object'])
+        X2 = X.select_dtypes(exclude=['object'])
+  
+        if sorted(X1)!=[]:
+           X1 = X1.apply(LabelEncoder().fit_transform)
+           X = pd.concat([X2,X1],axis=1)
+    
+        y = LabelEncoder().fit_transform(y)
+  
+     else:
+        st.write('Note: Please upload a csv file')
+  return X, y, X_name, X1
