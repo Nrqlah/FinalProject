cb9051e26af195fdabee74593683ea43659fe38a
Nrqlah <106502785+Nrqlah@users.noreply.github.com>
2022-06-15 18:44:19 +0800

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -6,26 +6,46 @@ st.write('''#Trying my best''')
 
 st.sidebar.header('User Input Parameter')
 st.sidebar.subheader('Please upload custom data in csv format')
+ 
+choice = st.sidebar.radio('Choose a dataset',('Default','User-defined'),index=0)
 
-upload_file = st.sidebar.file_uploader('Upload a csv',type='csv')
-if upload_file!=None:
-  st.write(uplod_file)
-  data = pd.read_csv(upload_file)
-  
-  y_name = st.sidebar.selectbox('Select a y variable',sorted(data))
-  X_name = st.sidebar.multiselect('Select the x variable(s)',
-                                  sorted(data)[1],
-                                  help='You may select more than one varible')
-  y = data.loc[:,y_name]
-  X = data.loc[:,X_name]
-  X1 = X.select_dtypes(include=['object'])
-  X2 = X.select_dtypes(exclude=['object'])
-  
-  if sorted(X1)!=[]:
-    X1 = X1.apply(LabelEncoder().fit_transform)
-    X = pd.concat([X2,X1],axis=1)
+def default_dataset(name):
+  data = None
+  if name == 'Iris':
+    data = dataset.load_iris()
+  else:
+    data = dataset.load_wine()
+  X = data.data
+  y = data.target
+  return X, y
     
-   y = LabelEncoder().fit_transform(y)
+
+def User_defined_dataset(chosen_name):
+  X = []
+  y = []
+  X_name = []
+  X1 = []
+
+  upload_file = st.sidebar.file_uploader('Upload a csv',type='csv')
+  if upload_file!=None:
+    st.write(uplod_file)
+    data = pd.read_csv(upload_file)
   
- else:
-  st.write('Note: Please upload a csv file')
+    y_name = st.sidebar.selectbox('Select a y variable',sorted(data))
+    X_name = st.sidebar.multiselect('Select the x variable(s)',
+                                   sorted(data)[1],
+                                    help='You may select more than one varible')  
+    y = data.loc[:,y_name]
+    X = data.loc[:,X_name]
+    X1 = X.select_dtypes(include=['object'])
+    X2 = X.select_dtypes(exclude=['object'])
+  
+    if sorted(X1)!=[]:
+      X1 = X1.apply(LabelEncoder().fit_transform)
+      X = pd.concat([X2,X1],axis=1)
+    
+    y = LabelEncoder().fit_transform(y)
+  
+  else:
+    st.write('Note: Please upload a csv file')
+ return X, y, X_name, X1
