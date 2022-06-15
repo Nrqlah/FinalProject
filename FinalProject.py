f783d96687e20916ca6ff70b464a310328284af2
Nrqlah <106502785+Nrqlah@users.noreply.github.com>
2022-06-15 19:19:04 +0800

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -56,4 +56,4 @@ def User_defined_dataset(chosen_name):
           st.write('Note: Please upload a csv file')
     return X, y, X_name, X1
 
-   X, y, X_name, cat_var = User_defined_dataset(chosen)
+X, y, X_name, cat_var = User_defined_dataset(chosen)
