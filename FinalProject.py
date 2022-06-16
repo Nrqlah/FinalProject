01ce447e84b7b9087d5052e243c2c4f6174ae7c0
Nrqlah <106502785+Nrqlah@users.noreply.github.com>
2022-06-16 08:32:42 +0800

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -61,8 +61,8 @@ def user_defined_dataset(category):
                   X1 = X1.apply(LabelEncoder().fit_transform)
                   X = pd.concat([X2,X1],axis=1)
            y = LabelEncoder().fit_transform(y)
-           else:
-               st.write('Note: Please upload a CSV file to continue this program.')
+        else:
+           st.write('Note: Please upload a CSV file to continue this program.')
 
     return X,y, X_features, X1
 
