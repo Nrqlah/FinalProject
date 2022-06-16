c08a8f770c46135c3292da12a2b89072418598ba
Nrqlah <106502785+Nrqlah@users.noreply.github.com>
2022-06-16 10:58:15 +0800

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -18,6 +18,8 @@ import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
+fixMatplotlib = true
+
 st.write('## FINAL PROJECT:')
 st.title('Classification Machine Learning Web App')
 st.write('### Bismillah')
@@ -167,3 +169,44 @@ if len(X)!=0 and len(y)!=0:
   st.write(df)
 else: 
    st.write('Note: No classification report generated.', unsafe_allow_html=True)
+
+
+#-----------------------------------------------------------
+# Matplot
+st.write("## 4: Principal Component Analysis Plot")
+suitable = 1
+if len(X_features) <2:
+  st.write("<font color='Aquamarine'>Note: No PCA plot as it requires at least two predictors.</font>", unsafe_allow_html=True)
+  suitable = 0
+else:
+    for names in X_features:
+        if names in cat_var:
+           st.write("<font color='Aquamarine'>Note: No PCA plot as it only supports numerical predictors.</font>", unsafe_allow_html=True)
+           suitable = 0
+           break
+
+if suitable == 1:
+   pca = PCA(2)
+   X_projected = pca.fit_transform(X)
+
+   x1 = X_projected[:, 0]
+   x2 = X_projected[:, 1]
+
+   fig = plt.figure()
+   plt.scatter(x1, x2,
+               c=y, alpha=0.8,
+               cmap='viridis')
+
+   plt.xlabel('Principal Component 1')
+   plt.ylabel('Principal Component 2')
+   plt.colorbar()
+   st.pyplot(fig)
+            
+            
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
